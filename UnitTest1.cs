1f0fe7c49d35f303de883702ed6fd1382dcbc1ee
amit_shinde3 <amit_shinde3@persistent.com>
2021-09-29 16:39:33 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using AzureProject01.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace AzureProject01
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            ImageController controller = new ImageController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            ImageController controller = new ImageController();
+
+            //Act
+            ViewResult result = controller.LatestImage() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
